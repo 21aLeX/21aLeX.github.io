6108918232de3341c75128dd35f905074fccc2a0
alex <aleksandra12mail@mail.ru>
2025-10-17 20:15:38 +0800

    feat: добавлен компонент основной информации

--- script.js
+++ script.js
@@ -15,44 +15,18 @@ function loadComponent(filePath, containerId) {
         });
 }
 
-loadComponent('./components/info/info.html', 'app');
-loadComponent('./components/contacts/contacts.html', 'app');
-setTimeout(()=>{
-    animOnScroll();
-}, 300);
-// document.addEventListener('DOMContentLoaded', animOnScroll);
-document.addEventListener("scroll", animOnScroll);
-    
-function animOnScroll(){
-    let animItems = document.querySelectorAll('._animItems');
-    if(animItems.length > 0){
-        for(let i = 0; i < animItems.length; i++){
-            const animItem = animItems[i];
-            const animItemHeight = animItem.offsetHeight;
-            const animItemOffset = offset(animItem).top;
-            const animStart = 4;
+const components = [
+    './components/info/info.html',
+    './components/contacts/contacts.html',
+    './components/description/description.html',
+    './components/skils/skils.html',
+    './components/experience/experience.html'
+];
 
-
-            let animItemPoint = window.innerHeight - animItemHeight/animStart;
-
-            if(animItemHeight >window.innerHeight){
-                animItemPoint = window.innerHeight - window.innerHeight/animStart;
-                 }
-
-                 if((pageYOffset > animItemOffset - animItemPoint)&&pageYOffset<(animItemOffset+animItemHeight)){
-                     animItem.classList.add('_active');
-                 }else{
-                     if(!animItem.classList.contains('_anim-no-hide')){
-
-                     animItem.classList.remove('_active');
-                     }
-                 }
-        }
-    }
-    function offset(el){
-        const rect = el.getBoundingClientRect(),
-        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return{top:rect.top +scrollTop, left: rect.left+scrollLeft}
+async function loadComponents() {
+    for (const component of components) {
+        await loadComponent(component, 'app');
     }
 }
+
+loadComponents();
